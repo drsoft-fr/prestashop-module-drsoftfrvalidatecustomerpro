1e6148b2b4869dc09a81cfbc7fdd51373859c81b
Dylan Ramos <dylan.ramos.n@gmail.com>
2024-10-15 00:28:05 +0200

    Add drsoftfrvalidatecustomerpro module
    Create a new PrestaShop module named drsoftfrvalidatecustomerpro to validate customers at registration and manage groups. Includes all necessary files for installation, enabling, disabling, and uninstallation, along with error handling and cache clearing mechanisms.

---
+++ drsoftfrvalidatecustomerpro.php
@@ -0,0 +1,241 @@
+<?php
+
+declare(strict_types=1);
+
+use DrSoftFr\Module\ValidateCustomerPro\Config;
+use PrestaShop\PrestaShop\Core\Cache\Clearer\CacheClearerChain;
+
+if (!defined('_PS_VERSION_') || !defined('_CAN_LOAD_FILES_')) {
+    exit;
+}
+
+$autoloadPath = __DIR__ . '/vendor/autoload.php';
+if (file_exists($autoloadPath)) {
+    require_once $autoloadPath;
+}
+
+/**
+ * Class drsoftfrvalidatecustomerpro
+ */
+class drsoftfrvalidatecustomerpro extends Module
+{
+    /**
+     * @var string $authorEmail Author email
+     */
+    public $authorEmail;
+
+    /**
+     * @var string $moduleGithubRepositoryUrl Module GitHub repository URL
+     */
+    public $moduleGithubRepositoryUrl;
+
+    /**
+     * @var string $moduleGithubIssuesUrl Module GitHub issues URL
+     */
+    public $moduleGithubIssuesUrl;
+
+    /**
+     * @var bool $isPsVersion8 Indicates whether the version of PrestaShop is 8 or not
+     */
+    public $isPsVersion8;
+
+    public function __construct()
+    {
+        $this->author = 'drSoft.fr';
+        $this->bootstrap = true;
+        $this->confirmUninstall = $this->trans('Are you sure you want to uninstall?', [], 'Modules.Drsoftfrvalidatecustomerpro.Admin');
+        $this->dependencies = [];
+        $this->description = $this->trans('Validate customer at registration and assign groups automatically.', [], 'Modules.Drsoftfrvalidatecustomerpro.Admin');
+        $this->displayName = $this->trans('drSoft.fr Validate customer Pro', [], 'Modules.Drsoftfrvalidatecustomerpro.Admin');
+        $this->name = 'drsoftfrvalidatecustomerpro';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = [
+            'min' => '1.7.8',
+            'max' => _PS_VERSION_
+        ];
+        $this->tab = 'administration';
+        $this->tabs = [];
+        $this->version = '0.0.1';
+        $this->authorEmail = 'contact@drsoft.fr';
+        $this->moduleGithubRepositoryUrl = 'https://github.com/drsoft-fr/prestashop-module-drsoftfrvalidatecustomerpro';
+        $this->moduleGithubIssuesUrl = 'https://github.com/drsoft-fr/prestashop-module-drsoftfrvalidatecustomerpro/issues';
+        $this->isPsVersion8 = (bool)version_compare(_PS_VERSION_, '8.0', '>=');
+
+        parent::__construct();
+    }
+
+    /**
+     * Disables the module.
+     *
+     * @param bool $force_all Whether to disable all instances of the module, even if they are currently enabled.
+     *
+     * @return bool Whether the module was disabled successfully.
+     */
+    public function disable($force_all = false)
+    {
+        if (!parent::disable($force_all)) {
+            $this->handleException(
+                new Exception(
+                    $this->trans(
+                        'An error has occurred when deactivating the module.',
+                        [],
+                        'Modules.Drsoftfrvalidatecustomerpro.Error'
+                    )
+                )
+            );
+
+            return false;
+        }
+
+        try {
+            $this->getCacheClearerChain()->clear();
+        } catch (Throwable $t) {
+            $this->handleException($t);
+        }
+
+        return true;
+    }
+
+    /**
+     * Enables the module by clearing the cache and calling the parent's enable method.
+     *
+     * @param bool $force_all Whether to force the enabling of all modules.
+     *
+     * @return bool True on successful enable, false otherwise.
+     */
+    public function enable($force_all = false)
+    {
+        if (!parent::enable($force_all)) {
+            $this->handleException(
+                new Exception(
+                    $this->trans(
+                        'An error has occurred when activating the module.',
+                        [],
+                        'Modules.Drsoftfrvalidatecustomerpro.Error'
+                    )
+                )
+            );
+
+            return false;
+        }
+
+        try {
+            $this->getCacheClearerChain()->clear();
+        } catch (Throwable $t) {
+            $this->handleException($t);
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the CacheClearerChain.
+     *
+     * @return CacheClearerChain
+     *
+     * @throws Exception
+     */
+    private function getCacheClearerChain(): CacheClearerChain
+    {
+        $cacheClearerChain = $this->get('prestashop.core.cache.clearer.cache_clearer_chain');
+
+        if (!($cacheClearerChain instanceof CacheClearerChain)) {
+            throw new Exception('The cacheClearerChain object must implement CacheClearerChain.');
+        }
+
+        return $cacheClearerChain;
+    }
+
+    /**
+     * Redirects the admin user to the ValidateCustomerPro controller in the admin panel.
+     *
+     * @return void
+     */
+    public function getContent(): void
+    {
+    }
+
+    /**
+     * Handles an exception by logging an error message.
+     *
+     * @param Throwable $t The exception to handle.
+     *
+     * @return void
+     */
+    private function handleException(Throwable $t): void
+    {
+        $errorMessage = Config::createErrorMessage(__METHOD__, __LINE__, $t);
+
+        PrestaShopLogger::addLog($errorMessage, 3);
+
+        $this->_errors[] = $errorMessage;
+    }
+
+    /**
+     * Installs the module
+     *
+     * @return bool Returns true if the installation is successful, false otherwise.
+     */
+    public function install(): bool
+    {
+        if (!parent::install()) {
+            $this->handleException(
+                new Exception(
+                    $this->trans(
+                        'There was an error during the installation.',
+                        [],
+                        'Modules.Drsoftfrvalidatecustomerpro.Error'
+                    )
+                )
+            );
+
+            return false;
+        }
+
+        try {
+            $this->getCacheClearerChain()->clear();
+        } catch (Throwable $t) {
+            $this->handleException($t);
+        }
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isUsingNewTranslationSystem(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Uninstalls the module
+     *
+     * @return bool Returns true if uninstallation was successful, false otherwise
+     */
+    public function uninstall(): bool
+    {
+        if (!parent::uninstall()) {
+            $this->handleException(
+                new Exception(
+                    $this->trans(
+                        'There was an error during the uninstallation.',
+                        [],
+                        'Modules.Drsoftfrvalidatecustomerpro.Error'
+                    )
+                )
+            );
+
+            return false;
+        }
+
+        try {
+            $this->getCacheClearerChain()->clear();
+        } catch (Throwable $t) {
+            $this->handleException($t);
+        }
+
+        return true;
+    }
+}
---
+++ src/Config.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrSoftFr\Module\ValidateCustomerPro;
+
+use Throwable;
+
+/**
+ * Class Config
+ *
+ * This class contains constants and a method to generate error messages.
+ */
+final class Config
+{
+    const ERROR_MESSAGE_PATTERN = 'drsoftfrvalidatecustomerpro - %s - %d - Throwable #%d - %s.';
+
+    /**
+     * Creates an error message using the given method, line number and throwable object.
+     *
+     * @param string $method The name of the method where the error occurred.
+     * @param int $line The line number where the error occurred.
+     * @param Throwable $t The throwable object representing the error.
+     *
+     * @return string The formatted error message.
+     */
+    static public function createErrorMessage(string $method, int $line, Throwable $t): string
+    {
+        return sprintf(self::ERROR_MESSAGE_PATTERN, $method, $line, $t->getCode(), $t->getMessage());
+    }
+}
