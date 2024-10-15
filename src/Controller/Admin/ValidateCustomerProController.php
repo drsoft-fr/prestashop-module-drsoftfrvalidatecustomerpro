3fe43dd87d4c2bef2f266539185da693d67c33f6
Dylan Ramos <dylan.ramos.n@gmail.com>
2024-10-15 16:51:49 +0200

    Enable reset default settings feature
    Re-enabled the reset button in the admin template and added a new route and controller action for resetting default settings. The new controller action handles resetting settings with appropriate success and error handling.

--- src/Controller/Admin/ValidateCustomerProController.php
+++ src/Controller/Admin/ValidateCustomerProController.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace DrSoftFr\Module\ValidateCustomerPro\Controller\Admin;
 
+use DrSoftFr\Module\ValidateCustomerPro\Data\Configuration\ValidateCustomerProConfiguration;
 use drsoftfrvalidatecustomerpro;
 use PrestaShop\PrestaShop\Core\Form\FormHandlerInterface;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use PrestaShopBundle\Security\Annotation\AdminSecurity;
 use PrestaShopBundle\Security\Annotation\ModuleActivated;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 /**
  * Class ValidateCustomerProController.
@@ -49,11 +52,63 @@ final class ValidateCustomerProController extends FrameworkBundleAdminController
     }
 
     /**
+     * Reset setting
+     *
+     * @AdminSecurity(
+     *     "is_granted('update', request.get('_legacy_controller'))",
+     *     redirectRoute="admin_drsoft_fr_validate_customer_pro_index",
+     *     message="You do not have permission to reset this."
+     * )
+     *
+     * @return RedirectResponse
+     */
+    public function resetAction(): RedirectResponse
+    {
+        try {
+            $this
+                ->getValidateCustomerProConfiguration()
+                ->initConfiguration();
+
+            $this->addFlash(
+                'success',
+                $this->trans(
+                    'The default setting are reset.',
+                    'Modules.Drsoftfrvalidatecustomerpro.Admin'
+                )
+            );
+        } catch (Throwable $t) {
+            $this->addFlash(
+                'error',
+                $this->trans(
+                    'Cannot reset the setting. Exception: #%code% - %message%',
+                    'Modules.Drsoftfrvalidatecustomerpro.Admin',
+                    [
+                        '%code%' => $t->getCode(),
+                        '%message%' => $t->getMessage(),
+                    ]
+                )
+            );
+        }
+
+        return $this->redirectToRoute('admin_drsoft_fr_validate_customer_pro_index');
+    }
+
+    /**
      * @return drsoftfrvalidatecustomerpro|object
      */
     protected function getModule()
     {
         return $this->get('drsoft_fr.module.validate_customer_pro.module');
+    }
+
+    /**
+     * Get ValidateCustomerPro configuration.
+     *
+     * @return ValidateCustomerProConfiguration
+     */
+    protected function getValidateCustomerProConfiguration(): ValidateCustomerProConfiguration
+    {
+        return $this->get('drsoft_fr.module.validate_customer_pro.data.configuration.validate_customer_pro_configuration');
     }
 
     /**
