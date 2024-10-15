7e0dad646a9c77c2507045f8b9b3789c49e426a3
Dylan Ramos <dylan.ramos.n@gmail.com>
2024-10-15 02:04:39 +0200

    Add exception classes for ValidateCustomerPro module
    Introduced `ValidateCustomerProException` as the base exception for the ValidateCustomerPro subdomain and `ValidateCustomerProConstraintException` for constraint violations.

---
+++ src/Exception/ValidateCustomerPro/ValidateCustomerProConstraintException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrSoftFr\Module\ValidateCustomerPro\Exception\ValidateCustomerPro;
+
+/**
+ * Thrown when ValidateCustomerPro constraints are violated
+ */
+class ValidateCustomerProConstraintException extends ValidateCustomerProException
+{
+}
---
+++ src/Exception/ValidateCustomerPro/ValidateCustomerProException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrSoftFr\Module\ValidateCustomerPro\Exception\ValidateCustomerPro;
+
+use Exception;
+
+/**
+ * Is base exception for ValidateCustomerProException subdomain
+ */
+class ValidateCustomerProException extends Exception
+{
+}
