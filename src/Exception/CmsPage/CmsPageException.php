f63aef92a4ce054d3337631e5048bcda6109222a
Dylan Ramos <dylan.ramos.n@gmail.com>
2024-10-16 04:47:53 +0200

    Add CMS Page exception classes
    Introduced CmsPageException as the base exception for handling CmsPage-related errors. Added specific exception classes CmsPageConstraintException and NonexistentCmsPageIdException to address constraint violations and non-existent page ID cases, respectively. A default index.php file was also included to adhere to PrestaShop's licensing and caching practices.

---
+++ src/Exception/CmsPage/CmsPageException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrSoftFr\Module\ValidateCustomerPro\Exception\CmsPage;
+
+use Exception;
+
+/**
+ * Is base exception for CmsPage subdomain
+ */
+class CmsPageException extends Exception
+{
+}
---
+++ src/Exception/CmsPage/NonexistentCmsPageIdException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrSoftFr\Module\ValidateCustomerPro\Exception\CmsPage;
+
+/**
+ * Throw when the identifier does not correspond to an existing CmsPage.
+ */
+class NonexistentCmsPageIdException extends CmsPageException
+{
+}
