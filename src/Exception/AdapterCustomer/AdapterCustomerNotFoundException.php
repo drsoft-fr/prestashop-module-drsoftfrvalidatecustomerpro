54dd275b73a6ee3dd94e306430248cc374ef491f
Dylan Ramos <dylan.ramos.n@gmail.com>
2024-10-23 16:54:29 +0200

    Add AdapterCustomer exceptions
    Introduced new exception classes for AdapterCustomer subdomain. This includes a base AdapterCustomerException and a specific AdapterCustomerNotFoundException to handle scenarios where an AdapterCustomer is not found.

---
+++ src/Exception/AdapterCustomer/AdapterCustomerNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrSoftFr\Module\ValidateCustomerPro\Exception\AdapterCustomer;
+
+/**
+ * Is thrown when AdapterCustomer is not found
+ */
+class AdapterCustomerNotFoundException extends AdapterCustomerException
+{
+}
